5eb97a7f0da86ef4b0003f4ec9adcb03be9369df
CallmePier <phunguyentrieu3@gmail.com>
2024-09-04 20:30:46 +0700

    - tinh gia tri trung binh

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int countNumber = 5;
+        int sum = 0;
+
+        for(int i =1; i < countNumber; i++){
+            System.out.println("Vui long nhap so nguyen thu: " + i );
+            int numb = sc.nextInt();
+            sum += numb;
+        }
+
+        double numAveg = (double) sum / countNumber;
+
+        System.out.println("Gia tri trung binh la: " + numAveg);
+
+    }
+}
